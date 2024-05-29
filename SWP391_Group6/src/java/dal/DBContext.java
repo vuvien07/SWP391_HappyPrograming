fc7a009865887a13e333de3e337d1e7f3264d519
Đào Nhật Minh <155622151+MinhDNHE176775@users.noreply.github.com>
2024-05-29 11:05:40 +0700

    Test commit
    test

--- SWP391_Group6/src/java/dal/DBContext.java
+++ SWP391_Group6/src/java/dal/DBContext.java
@@ -32,7 +32,9 @@ public abstract class DBContext<T> {
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
+        System.out.println("Vu Non VCL");
     }
+    
 
     public abstract ArrayList<T> listAll();
 
